5de81c9ad33175cff750ce67cf199d23c69d327a
Karl Robinson <karl.robinson@ctrl-hub.com>
2024-12-04 08:55:31 +0000

    fix mappings

--- src/models/Vehicle.ts
+++ src/models/Vehicle.ts
@@ -29,9 +29,9 @@ export class Vehicle implements Model {
             vehicle.id = data.id || '';
             vehicle.type = data.type || 'vehicles';
             vehicle.relationships = data.relationships || {};
-            vehicle.attributes.registration = vehicle.relationships.author?.data?.id || '';
-            vehicle.attributes.vin = vehicle.relationships.form?.data?.id || '';
-            vehicle.attributes.description = vehicle.relationships.form_version?.data?.id || '';
+            vehicle.attributes.registration = data.attributes.registration || '';
+            vehicle.attributes.vin = data.attributes.vin || '';
+            vehicle.attributes.description = data.attributes.description || '';
             vehicle.meta = data.meta || {};
             vehicle.links = data.links || {};
         }
