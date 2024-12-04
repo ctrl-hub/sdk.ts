959a54a435ce4353ca4f71c857f1c947e00ed5e9
Johnny Greaves <jgreaves8@gmail.com>
2024-12-04 09:16:00 +0000

    Build

--- dist/index.js
+++ dist/index.js
@@ -688,9 +688,9 @@ class Vehicle {
       vehicle.id = data.id || "";
       vehicle.type = data.type || "vehicles";
       vehicle.relationships = data.relationships || {};
-      vehicle.attributes.registration = vehicle.relationships.author?.data?.id || "";
-      vehicle.attributes.vin = vehicle.relationships.form?.data?.id || "";
-      vehicle.attributes.description = vehicle.relationships.form_version?.data?.id || "";
+      vehicle.attributes.registration = data.attributes.registration || "";
+      vehicle.attributes.vin = data.attributes.vin || "";
+      vehicle.attributes.description = data.attributes.description || "";
       vehicle.meta = data.meta || {};
       vehicle.links = data.links || {};
     }
