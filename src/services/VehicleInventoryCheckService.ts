cf90c048b0b1026fc67d54243e229352a40bbf02
Peter Ruczynski <peter@ctrl-hub.com>
2025-02-06 15:11:34 +0000

    New service for inventory checks

---
+++ src/services/VehicleInventoryCheckService.ts
@@ -0,0 +1,9 @@
+import {Client} from "Client";
+import {BaseService} from "./BaseService";
+import {VehicleInventoryCheck} from "@models/VehicleInventoryCheck";
+
+export class VehicleInventoryCheckService extends BaseService<VehicleInventoryCheck> {
+    constructor(client: Client) {
+        super(client, "/v3/orgs/:orgId/assets/vehicles/inventory-checks");
+    }
+}
