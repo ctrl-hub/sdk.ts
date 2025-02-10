df61395430499e9561162a9b36f51eb7c4491cd0
Karl Robinson <karl.robinson@ctrl-hub.com>
2025-02-10 11:35:47 +0000

    fix properties service model

--- src/services/PropertiesService.ts
+++ src/services/PropertiesService.ts
@@ -1,16 +1,16 @@
 import {Client} from "Client";
 import {BaseService} from "./BaseService";
-import {Equipment} from "../models/Equipment";
 import type { InternalResponse } from '../types/Response';
+import type { Property } from "@models/Property";
 
-export class PropertiesService extends BaseService<Equipment> {
+export class PropertiesService extends BaseService<Property> {
     constructor(client: Client) {
         super(client, "/v3/governance/properties");
     }
 
-    async byUprn(uprn: string): Promise<InternalResponse<Equipment>>;
-    async byUprn(uprn: string[]): Promise<InternalResponse<Equipment[]>>;
-    async byUprn(uprn: string|string[]): Promise<InternalResponse<Equipment | Equipment[]>> {
+    async byUprn(uprn: string): Promise<InternalResponse<Property>>;
+    async byUprn(uprn: string[]): Promise<InternalResponse<Property[]>>;
+    async byUprn(uprn: string|string[]): Promise<InternalResponse<Property | Property[]>> {
         // only 1 uprn, append directly to endpoint i.e. /properties/{uprn}
         if (!Array.isArray(uprn)) {
             return await this.client.makeGetRequest(`${this.endpoint}/${uprn}`);
