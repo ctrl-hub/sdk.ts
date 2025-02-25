80c11590cda94099ebfb06688c6bf129931e0906
Johnny Greaves <jgreaves8@gmail.com>
2025-02-25 11:22:01 +0000

    Tests for Equipment model

--- tests/utils/JsonSerializer/EquipmentAndEquipmentModel.test.ts
+++ tests/utils/JsonSerializer/EquipmentAndEquipmentModel.test.ts
@@ -9,34 +9,6 @@ describe('JsonApiSerializer for Equipment and Model', () => {
     const hydrator = new Hydrator();
 
     describe('buildCreatePayload', () => {
-        it('should transform equipment input to correct JSONAPI format', () => {
-
-            const equipment = new Equipment();
-            equipment.serial = "TestSerial";
-            equipment.model = "a354c5cc-2c9a-44fa-80f6-de3d97946ccb";
-
-            const payload = (new JsonApiSerializer(hydrator.getModelMap())).buildCreatePayload(equipment);
-
-            let expectedPayload = {
-                data: {
-                    type: "equipment-items",
-                    attributes: {
-                        serial: "TestSerial"
-                    },
-                    relationships: {
-                        model: {
-                            data: {
-                                type: "equipment-models",
-                                id: "a354c5cc-2c9a-44fa-80f6-de3d97946ccb"
-                            }
-                        }
-                    }
-                }
-            }
-
-            // Exact expected output format
-            expect(payload).toEqual(expectedPayload);
-        });
 
         it('should transform a model whcih does not have jsonApiMapping', () => {
             const formCategory = new FormCategory();
