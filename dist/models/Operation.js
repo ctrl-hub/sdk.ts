d65e6e5042a5a4550754ae6cadafcbcaa51d20d6
Karl Robinson <karl.robinson@ctrl-hub.com>
2025-03-21 15:21:34 +0000

    add operation team patching

--- dist/models/Operation.js
+++ dist/models/Operation.js
@@ -28,6 +28,11 @@ export class Operation extends BaseModel {
             type: 'array',
             modelType: 'users',
         },
+        {
+            name: 'teams',
+            type: 'array',
+            modelType: 'teams',
+        },
     ];
     constructor(data) {
         super(data);
--- dist/services/OperationsService.js
+++ dist/services/OperationsService.js
@@ -2,6 +2,7 @@ import { Client } from "../Client";
 import { BaseService } from "./BaseService";
 import { User } from "../models/User";
 import { JsonApiSerializer } from "../utils/JsonSerializer";
+import { Team } from "../models/Team";
 export class OperationsService extends BaseService {
     constructor(client, schemeId, workOrderId, operationId) {
         super(client, operationId ? `/v3/orgs/:orgId/governance/schemes/${schemeId}/work-orders/${workOrderId}/operations/${operationId}` : `/v3/orgs/:orgId/governance/schemes/${schemeId}/work-orders/${workOrderId}/operations`);
@@ -10,5 +11,10 @@ export class OperationsService extends BaseService {
         const jsonApiSerializer = new JsonApiSerializer(this.hydrator.getModelMap());
         const payload = jsonApiSerializer.buildRelationshipPayload(new User, users);
         return await this.client.makePatchRequest(`${this.endpoint}/relationships/assignees`, payload);
+    }
+    async patchTeams(teams) {
+        const jsonApiSerializer = new JsonApiSerializer(this.hydrator.getModelMap());
+        const payload = jsonApiSerializer.buildRelationshipPayload(new Team, teams);
+        return await this.client.makePatchRequest(`${this.endpoint}/relationships/teams`, payload);
     }
 }
